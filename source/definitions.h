b144d6706ad65a6cfe68a89965b0e4241ee0c49d
Zbizu <zbirox@gmail.com>
2022-07-31 20:57:42 +0200

    bump editor version

--- source/definitions.h
+++ source/definitions.h
@@ -24,7 +24,7 @@
 // xxyyzzt (major, minor, subversion)
 #define __RME_VERSION_MAJOR__      3
 #define __RME_VERSION_MINOR__      8
-#define __RME_SUBVERSION__         3
+#define __RME_SUBVERSION__         4
 
 #define __LIVE_NET_VERSION__       5
 
