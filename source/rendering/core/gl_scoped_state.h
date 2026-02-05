25e4ef1123f9d7171e87577642134fadeec1e7a3
glaszczkoldre <karolak6612@gmail.com>
2026-02-05 14:25:23 +0100

    Update gl_scoped_state.h

--- source/rendering/core/gl_scoped_state.h
+++ source/rendering/core/gl_scoped_state.h
@@ -3,16 +3,24 @@
 
 #include <glad/glad.h>
 
-// RAII wrapper for glEnable/glDisable
+/**
+ * @brief RAII wrapper for glEnable/glDisable
+ *
+ * Intended for short-lived scope-based state changes, not for persistent state modifications.
+ */
 class ScopedGLCapability {
 public:
-	explicit ScopedGLCapability(GLenum capability, bool enable = true) :
+	[[nodiscard]] explicit ScopedGLCapability(GLenum capability, bool enable = true) :
 		capability_(capability) {
 		was_enabled_ = glIsEnabled(capability);
 		if (enable) {
-			if (!was_enabled_) glEnable(capability);
+			if (!was_enabled_) {
+				glEnable(capability);
+			}
 		} else {
-			if (was_enabled_) glDisable(capability);
+			if (was_enabled_) {
+				glDisable(capability);
+			}
 		}
 	}
 
@@ -32,31 +40,23 @@ private:
 	GLboolean was_enabled_;
 };
 
-// RAII wrapper for glBlendFunc and glBlendEquation
+/**
+ * @brief RAII wrapper for glBlendFunc and glBlendEquation
+ *
+ * Intended for short-lived scope-based state changes, not for persistent state modifications.
+ */
 class ScopedGLBlend {
 public:
-	ScopedGLBlend(GLenum sfactor, GLenum dfactor) {
-		glGetIntegerv(GL_BLEND_SRC_RGB, &prev_src_rgb_);
-		glGetIntegerv(GL_BLEND_DST_RGB, &prev_dst_rgb_);
-		glGetIntegerv(GL_BLEND_SRC_ALPHA, &prev_src_alpha_);
-		glGetIntegerv(GL_BLEND_DST_ALPHA, &prev_dst_alpha_);
-		glGetIntegerv(GL_BLEND_EQUATION_RGB, &prev_eq_rgb_);
-		glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &prev_eq_alpha_);
-
+	[[nodiscard]] ScopedGLBlend(GLenum sfactor, GLenum dfactor) {
+		save_state();
 		glBlendFunc(sfactor, dfactor);
 	}
 
-    ScopedGLBlend(GLenum sfactor, GLenum dfactor, GLenum equation) {
-		glGetIntegerv(GL_BLEND_SRC_RGB, &prev_src_rgb_);
-		glGetIntegerv(GL_BLEND_DST_RGB, &prev_dst_rgb_);
-		glGetIntegerv(GL_BLEND_SRC_ALPHA, &prev_src_alpha_);
-		glGetIntegerv(GL_BLEND_DST_ALPHA, &prev_dst_alpha_);
-		glGetIntegerv(GL_BLEND_EQUATION_RGB, &prev_eq_rgb_);
-		glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &prev_eq_alpha_);
-
-        glBlendFunc(sfactor, dfactor);
-        glBlendEquation(equation);
-    }
+	[[nodiscard]] ScopedGLBlend(GLenum sfactor, GLenum dfactor, GLenum equation) {
+		save_state();
+		glBlendFunc(sfactor, dfactor);
+		glBlendEquation(equation);
+	}
 
 	~ScopedGLBlend() {
 		glBlendFuncSeparate(prev_src_rgb_, prev_dst_rgb_, prev_src_alpha_, prev_dst_alpha_);
@@ -67,6 +67,15 @@ public:
 	ScopedGLBlend& operator=(const ScopedGLBlend&) = delete;
 
 private:
+	void save_state() {
+		glGetIntegerv(GL_BLEND_SRC_RGB, &prev_src_rgb_);
+		glGetIntegerv(GL_BLEND_DST_RGB, &prev_dst_rgb_);
+		glGetIntegerv(GL_BLEND_SRC_ALPHA, &prev_src_alpha_);
+		glGetIntegerv(GL_BLEND_DST_ALPHA, &prev_dst_alpha_);
+		glGetIntegerv(GL_BLEND_EQUATION_RGB, &prev_eq_rgb_);
+		glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &prev_eq_alpha_);
+	}
+
 	GLint prev_src_rgb_, prev_dst_rgb_;
 	GLint prev_src_alpha_, prev_dst_alpha_;
 	GLint prev_eq_rgb_, prev_eq_alpha_;
